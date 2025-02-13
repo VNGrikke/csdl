87167c6050e87199f772f454a12aa7a7a72ae00e
VNGrikke <enlove2005@gmail.com>
2025-02-13 11:33:43 +0700

    ss10-update b10

--- ss10/b10.sql
+++ ss10/b10.sql
@@ -13,24 +13,24 @@ create index idx_city_name on world.city(Name);
 
 
 delimiter //
-	create procedure GetSpecialCountriesAndCities (in language_name char(30))
-	begin
-		select 
-			ct.Name as CountryName,
-			c.Name as CityName,
-			c.Population as CityPopulation,
-			sum(c.Population) over (partition by ct.Code) as TotalPopulation
-		from world.country ct
-		join world.city c on c.CountryCode = ct.Code
-		join world.countrylanguage ctlg on ctlg.CountryCode = ct.Code
-		where ctlg.Language = language_name and ctlg.IsOfficial = 'T' and c.Name like 'New%'
-		group by ct.Code, ct.Name, c.Name, c.Population
-		having sum(c.Population) over (partition by ct.Code) > 5000000
-		order by TotalPopulation desc
-		limit 10;
+create procedure getspecialcountriesandcities (in language_name varchar(50))
+begin
+    select 
+        ct.name as countryname, 
+        c.name as cityname, 
+        c.population as citypopulation
+    from world.country ct
+    join world.city c on c.countrycode = ct.code
+    join world.countrylanguage ctlg on ctlg.countrycode = ct.code
+    where ctlg.language = language_name
+        and c.name like 'new%'
+        and ct.Population > 5000000
+    order by citypopulation desc
+    limit 10;
 end;
 // delimiter ;
 
+
 call GetSpecialCountriesAndCities('English');
 
-
+drop procedure GetSpecialCountriesAndCities;
